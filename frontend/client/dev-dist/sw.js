afa55015c8f36149551102a356db12be815776df
Sunidhichaudhary21 <csunidhi22@gmail.com>
2026-08-24 08:40:53 +0530

    increse the size of card of product section..

--- frontend/client/dev-dist/sw.js
+++ frontend/client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-fdf26176'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.do5qk7urrog"
+    "revision": "0.ja3p5evhp4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
